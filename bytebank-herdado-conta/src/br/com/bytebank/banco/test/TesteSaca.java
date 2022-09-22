d244fa0135347345477cbdae542fe54711f06fb6
violiveiradev <violiveira.dev@gmail.com>
2022-09-22 14:35:46 -0300

    Primeiro commit

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteSaca.java
@@ -0,0 +1,23 @@
+package br.com.bytebank.banco.test;
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
+
+public class TesteSaca {
+
+    public static void main(String[] args) {
+        Conta conta = new ContaCorrente(123, 321);
+
+        conta.deposita(200.0);
+
+        try {
+            conta.saca(210.0); //SaldoInsuficienteException
+            //conta.saca(190.0); //Executa sem exception
+        } catch(SaldoInsuficienteException ex) {
+            System.out.println("Exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+
+        System.out.println(conta.getSaldo());
+    }
+}
