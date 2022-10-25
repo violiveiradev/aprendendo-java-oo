719226544ac6f4d5aca8f0a8b999f259e1a876f4
violiveiradev <violiveira.dev@gmail.com>
2022-10-24 22:59:50 -0300

    Conclusao entendendo java.util

--- bytebank-herdado-conta/src/br/com/bytebank/banco/model/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/model/Conta.java
@@ -1,7 +1,7 @@
 package br.com.bytebank.banco.model;
 
 
-public abstract class Conta {
+public abstract class Conta implements Comparable<Conta> {
 
     protected double saldo;
     private int agencia;
@@ -133,8 +133,13 @@ public abstract class Conta {
     }
 
     @Override
+    public int compareTo(Conta outra) {
+        return Double.compare(this.saldo, outra.saldo);
+    }
+
+    @Override
     public String toString() {
-        return "Numero: " + this.numero + ", Agencia: " + this.agencia;
+        return "Numero: " + this.numero + ", Agencia: " + this.agencia + ", Saldo: " + this.saldo;
     }
 
 }
