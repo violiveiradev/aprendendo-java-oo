8046b4b844f150246d148090db6e22ac99a3b836
violiveiradev <violiveira.dev@gmail.com>
2023-02-07 23:18:28 -0300

    Mapas

--- java-collections/src/br/com/alura/Curso.java
+++ java-collections/src/br/com/alura/Curso.java
@@ -1,9 +1,11 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Curso {
@@ -13,6 +15,7 @@ public class Curso {
     private int tempoTotal; // Inserindo variavel para soma de tempo (1 maneira)
     private List<Aula> aulas = new LinkedList<Aula>();
     private Set<Aluno> alunos = new HashSet<>();
+    private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
     
     public Curso(String nome, String instrutor) {
         this.nome = nome;
@@ -43,6 +46,7 @@ public class Curso {
 
     public void matricula(Aluno aluno){
         this.alunos.add(aluno);
+        this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
     }
 
     public Set<Aluno> getAlunos() {
@@ -66,6 +70,10 @@ public class Curso {
 
     public boolean estaMatriculado(Aluno aluno) {
         return this.alunos.contains(aluno);
+    }
+
+    public Aluno buscaMatriculado(int numero) {
+        return this.matriculaParaAluno.get(numero);
     }
     
 }
