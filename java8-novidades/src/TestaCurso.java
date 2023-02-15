6143957e9feab2d348e8b6999958b48de3a3a359
violiveiradev <violiveira.dev@gmail.com>
2023-02-15 16:08:52 -0300

    Mais Streams Collectors e APIs

--- java8-novidades/src/TestaCurso.java
+++ java8-novidades/src/TestaCurso.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 //import java.util.Comparator;
 import java.util.List;
-//import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 import static java.util.Comparator.*;
 
@@ -20,18 +20,36 @@ public class TestaCurso {
 
         cursos.sort(comparing(Curso::getNome));
 
+        System.out.println();
+        cursos.stream()
+            .filter(c -> c.getAlunos() >= 100)
+            .mapToInt(Curso::getAlunos)
+            .average()
+            .getAsDouble();
+
+       
+
         // cursos.stream()
         //     .filter(c -> c.getAlunos() >= 50)
         //     .map(c -> c.getAlunos())
         //     .forEach(c -> System.out.println(c));
+
+        // cursos.stream()
+        //     .filter(c -> c.getAlunos() >= 50)
+        //     .map(Curso::getAlunos)
+        //     .forEach(System.out::println);
+
         cursos.stream()
-            .filter(c -> c.getAlunos() >= 50)
-            .map(Curso::getAlunos)
-            .forEach(System.out::println);
+            .filter(c -> c.getAlunos() > 100)
+            .findFirst()
+            .ifPresent(c -> System.out.println(c.getNome()));
+        
+        List<Curso> cursosFiltrados = cursos.stream()
+            .filter(c -> c.getAlunos() > 50)
+            .collect(Collectors.toList());
 
-
-        // Stream<String> nomes = cursos.stream().map(Curso::getNome);
-        // System.out.println(nomes);
+        System.out.println(cursosFiltrados);
+        
     }
 }
 
